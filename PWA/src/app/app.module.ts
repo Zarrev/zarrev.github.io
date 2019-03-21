f7a95797527f2f555b5e8e903d8311f5b46763df
Zarrev <barko.zsolte@gmail.com>
2019-03-21 15:09:55 +0100

    Hello word app, base

---
+++ PWA/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+/*
+Because we call the .forRoot() method for each module
+ (due the ngx-bootstrap module providers), the functionalities will
+  be available in all components and modules of your project (global scope).
+ */
+
+export class AppModule { }
