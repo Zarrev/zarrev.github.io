00de94affb2dafc5ae00e784aa7bd1381a2bbf41
Zarrev <barko.zsolte@gmail.com>
2019-04-05 16:28:14 +0200

    Auth develop has started

--- WorldOfYourMeals/src/app/app.module.ts
+++ WorldOfYourMeals/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
