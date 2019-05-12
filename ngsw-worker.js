a08d82638a000333d7266b8e5a43ffd20be2d010
Zarrev <barko.zsolte@gmail.com>
2019-05-12 02:05:34 +0200

    auth persistance test + service worker (bug-fix)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -436,7 +436,7 @@
             const parsed = this.adapter.parseUrl(url, this.scope.registration.scope);
             if (parsed.origin === this.origin) {
                 // The URL is relative to the SW's origin domain.
-                return parsed.path;
+                /*return parsed.path;*/ return url; // overriding default @angular/service-worker URL behavior, to handle routing bug angular/angular #21636
             }
             else {
                 return url;
@@ -2422,7 +2422,7 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
                     // The latest manifest is broken. This means that new clients are at the mercy of the
                     // network, but caches continue to be valid for previous versions. This is
                     // unfortunate but unavoidable.
-                    this.state = DriverReadyState.EXISTING_CLIENTS_ONLY;
+                    /*this.state = DriverReadyState.EXISTING_CLIENTS_ONLY;*/ // removing EXISTING_CLIENTS_ONLY state, as it behaves incorrectly in offline testing, both locally & on GitHub pages
                     this.stateMessage = `Degraded due to: ${errorToString(err)}`;
                     // Cancel the binding for these clients.
                     Array.from(this.clientVersionMap.keys())
@@ -2488,7 +2488,7 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
                 }
                 catch (err) {
                     this.debugger.log(err, `Error occurred while updating to manifest ${hash}`);
-                    this.state = DriverReadyState.EXISTING_CLIENTS_ONLY;
+                    /*this.state = DriverReadyState.EXISTING_CLIENTS_ONLY;*/ // removing EXISTING_CLIENTS_ONLY state, as it behaves incorrectly in offline testing, both locally & on GitHub pages
                     this.stateMessage = `Degraded due to failed initialization: ${errorToString(err)}`;
                     return false;
                 }
